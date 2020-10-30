1d9c541a679a16003dee1f19154ae7e3501d6378
og-analytics <og-analytics@oilgainsanalytics.com>
2020-10-30 10:29:51 -0500

    control the Rmarkdown presentation on each document

---
+++ before-each-chapter.R
@@ -0,0 +1,15 @@
+set.seed(2020)
+
+options(digits = 3)
+knitr::opts_chunk$set(
+  comment = "#:>",
+  echo = TRUE,
+  error = TRUE,
+  out.width = "90%",
+  fig.align = "center",
+  fig.width = 7,
+  fig.asp = 0.618,
+  results = "hold",
+  fig.show = "hold",
+  collapse = FALSE
+)
